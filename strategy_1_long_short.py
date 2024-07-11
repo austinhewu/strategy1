c1ec4a27d6dbf1e37cc50c3cf756e2d6f6105ac2
austinhewu <austinhewu@gmail.com>
2024-07-11 17:56:51 -0400

    Update strategy_1_long_short.py

--- strategy_1_long_short.py
+++ strategy_1_long_short.py
@@ -36,7 +36,7 @@ def strategy_1(df):
     account_balance = 0
     df['year'] = pd.to_datetime(df['DATE TIME']).dt.year
 
-    for i in range(1, len(df)):
+    for i in range(2, len(df)):
         signal_2sd = 0
         signal_3sd = 0
         buy_signals = 0
@@ -57,9 +57,8 @@ def strategy_1(df):
         early_7 = False
         trend_change = False
 
-        if df.loc[i, 'TREND'] == 'Long':
-
-            if df.loc[i - 1, 'TREND'] == 'Short':
+        if df.loc[i - 1, 'TREND'] == 'Long':
+            if df.loc[i - 2, 'TREND'] == 'Short':
                 trend_change = True
                 if df.loc[i - 1, 'position'] > 0:  # close positions when trend changes
                     exit_price_pchange1 = df.loc[i, 'close']
@@ -79,7 +78,6 @@ def strategy_1(df):
                     signal_2sd += 1
                 elif tup[1] == '3SD':
                     signal_3sd += 1
-
 
             # entry signals
             if df.loc[i - 1, 'position'] < 3 and not pause and not doubled:
@@ -124,7 +122,7 @@ def strategy_1(df):
                         pause = True
                         triggered_5_or_6 = False
                         doubled = False
-                        df.loc[i, 'signal_type'] = df.loc[i, 'signal_type'] + '7'
+                        df.loc[i, 'signal_type'] = df.loc[i, 'signal_type'] + 'e7'
 
             # update position with buy signals
             if df.loc[i - 1, 'position'] + buy_signals <= 3:
@@ -219,8 +217,8 @@ def strategy_1(df):
                 pl_5 = exit_price_5 - df.loc[i, 'cost']
                 entry_prices.pop()
             if exit_price_7 != 0 and entry_prices and not early_7:
-                pl_7 = exit_price_7 - df.loc[i, 'cost']
-                entry_prices.pop()
+                pl_7 = (exit_price_7 - df.loc[i, 'cost']) * len(entry_prices)
+                entry_prices = []
             if exit_price_8 != 0 and entry_prices:
                 pl_8 = exit_price_8 - df.loc[i, 'cost']
                 entry_prices.pop()
@@ -237,8 +235,8 @@ def strategy_1(df):
             else:
                 df.loc[i, 'yearly_%P/L'] = 0
 
-        elif df.loc[i, 'TREND'] == 'Short':
-            if df.loc[i - 1, 'TREND'] == 'Long':
+        elif df.loc[i - 1, 'TREND'] == 'Short':
+            if df.loc[i - 2, 'TREND'] == 'Long':
                 trend_change = True
                 if df.loc[i - 1, 'position'] > 0:  # close positions when trend changes
                     exit_price_pchange1 = df.loc[i, 'close']
@@ -271,7 +269,7 @@ def strategy_1(df):
                     triggered_5_or_6 = False
                 elif df.loc[i, 'high'] >= df.loc[i, 'upper_band_2SD'] and signal_2sd < 2 and len(
                         entry_prices) < 3:  # [1]
-                    entry_price = df.loc[i, 'lower_band_2SD']
+                    entry_price = df.loc[i, 'upper_band_2SD']
                     buy_signals += 1
                     signal_2sd += 1
                     entry_prices.append((entry_price, '2SD'))
@@ -288,7 +286,7 @@ def strategy_1(df):
                     triggered_5_or_6 = False
                 elif df.loc[i, 'high'] >= df.loc[i, 'upper_band_3SD'] and signal_3sd < 2 and len(
                         entry_prices) < 3:  # [3]
-                    entry_price = df.loc[i, 'lower_band_3SD']
+                    entry_price = df.loc[i, 'upper_band_3SD']
                     buy_signals += 1
                     signal_3sd += 1
                     entry_prices.append((entry_price, '3SD'))
@@ -302,7 +300,7 @@ def strategy_1(df):
                         pause = True
                         triggered_5_or_6 = False
                         doubled = False
-                        df.loc[i, 'signal_type'] = df.loc[i, 'signal_type'] + '7'
+                        df.loc[i, 'signal_type'] = df.loc[i, 'signal_type'] + 'e7'
 
             # update position with buy signals
             if df.loc[i - 1, 'position'] + buy_signals <= 3:
@@ -345,7 +343,7 @@ def strategy_1(df):
                     df.loc[i, 'signal_type'] = df.loc[i, 'signal_type'] + '6'
                 elif df.loc[i, 'low'] <= df.loc[i, 'lower_band_2SD'] and not doubled:  # [5]
                     sell_signals += math.ceil(df.loc[i, 'position'] / 2)
-                    exit_price_5 = df.loc[i, 'upper_band_2SD']
+                    exit_price_5 = df.loc[i, 'lower_band_2SD']
                     if df.loc[i - 1, 'position'] == 3:
                         double = True
                     if df.loc[i - 1, 'position'] == 2 or df.loc[i - 1, 'position'] == 3:
@@ -354,7 +352,7 @@ def strategy_1(df):
                     pause = False
                     df.loc[i, 'signal_type'] = df.loc[i, 'signal_type'] + '5'
                 if df.loc[i, 'high'] >= df.loc[i, 'cost'] * (1 + 0.00618) and not pause:  # [7]
-                    sell_signals += 1
+                    sell_signals += df.loc[i, 'position']
                     exit_price_7 = df.loc[i, 'cost'] * (1 + 0.00618)
                     pause = True
                     triggered_5_or_6 = False
@@ -396,14 +394,14 @@ def strategy_1(df):
             elif exit_price_5 != 0 and not double and entry_prices:
                 pl_5 = exit_price_5 - df.loc[i, 'cost']
                 entry_prices.pop()
-            if exit_price_7 != 0 and entry_prices:
-                pl_7 = exit_price_7 - df.loc[i, 'cost']
-                entry_prices.pop()
+            if exit_price_7 != 0 and entry_prices and not early_7:
+                pl_7 = (exit_price_7 - df.loc[i, 'cost']) * len(entry_prices)
+                entry_prices = []
             if exit_price_8 != 0 and entry_prices:
                 pl_8 = exit_price_8 - df.loc[i, 'cost']
                 entry_prices.pop()
 
-            df.loc[i, 'P/L'] = pl_5 + pl_6 + pl_7 + pl_8 + pl_pchange1 + pl_pchange2
+            df.loc[i, 'P/L'] = -(pl_5 + pl_6 + pl_7 + pl_8 + pl_pchange1 + pl_pchange2)
 
             if account_balance == 0:
                 df.loc[i, '%P/L'] = df.loc[i, 'P/L'] / (df.loc[22, 'open'] * 3) * 100
